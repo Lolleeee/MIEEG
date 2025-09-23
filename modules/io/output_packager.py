4b55c695599c58df572805a822b8be9a3760b495
Lolleeee <bianchi.lolle@gmail.com>
2025-09-23 13:04:45 +0200

    test

--- modules/io/output_packager.py
+++ modules/io/output_packager.py
@@ -6,9 +6,21 @@ def package_eeg_data(eeg_data: np.ndarray) -> np.ndarray:
 
 
 class OutputPackager:
-    def __init__(self, eeg_data: np.ndarray):
+    def __init__(self, eeg_data, sensor_data, out_format: str = 'npz'):
         self.eeg_data = eeg_data
+        self.sensor_data = sensor_data
+        self.package = self.package_builder(self.eeg_data, self.sensor_data)
+        self.format = out_format
 
-    def package(self) -> np.ndarray:
-        packaged_data = package_eeg_data(self.eeg_data)
-        return packaged_data
+    def package_builder(self, eeg_data, sensor_data):
+        return {'eeg': eeg_data, 'sens': sensor_data}
+    
+    def _save_package(self, filename):
+        if self.format == 'npy':
+            np.save(filename, self.package)
+        elif self.format == 'npz':
+            np.savez(filename, **self.package)
+        else:
+            raise ValueError(f"Unsupported format: {self.format}")
+
+    def _iterate_segments(self, )
