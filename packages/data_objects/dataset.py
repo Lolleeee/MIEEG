Precision fix

packages/data_objects/dataset.py
@@ -176,9 +176,8 @@ class TorchDataset(Dataset, BasicDataset):
         return data
 
     def _normalize_item(self, item):
-        mean = self._norm_params[0].to(item.dtype)  
-        std = self._norm_params[1].to(item.dtype)   
-        
+        mean = self._norm_params[0]  
+        std = self._norm_params[1]   
         # Use appropriate epsilon for float16
         eps = 1e-5 if item.dtype == torch.float16 else 1e-10
         
@@ -200,6 +199,7 @@ class CustomTestDataset(Dataset, BasicDataset):
         BasicDataset.__init__(self, root_folder, unpack_func, precision=precision)
         self.nsamples = nsamples
         self.shape = shape
+        
         
 
         if self.root_folder is not None and os.path.isdir(self.root_folder):

packages/io/file_loader.py
@@ -44,15 +44,15 @@ def _calc_norm_params(train_loader, axes):
     n = 0
     mean = None
     M2 = None
-    
+    # Ensure calculations are in float32 to avoid precision issues
     for batch in tqdm(train_loader, desc="Calculating global parameters"):
         # Handle tuple outputs
         if isinstance(batch, (list, tuple)):
             batch = batch[0]
         
         if isinstance(batch, np.ndarray):
-            batch = torch.from_numpy(batch).float()
-        
+            batch = torch.from_numpy(batch)
+        batch = batch.float()
         batch_size = batch.size(0)
         if item_shape is None:
             item_shape = batch.shape[0:]
@@ -72,7 +72,7 @@ def _calc_norm_params(train_loader, axes):
             mean = (n * mean + batch_size * batch_mean) / new_n
             M2 = M2 + batch_var * batch_size + delta ** 2 * n * batch_size / new_n
             n = new_n
-    
+        print(f"Intermediate mean float32: {mean.flatten()}")
     if n < 2:
         return mean, torch.zeros_like(mean)
     
@@ -124,8 +124,9 @@ def get_data_loaders(
         )
         
         mean, std = _calc_norm_params(temp_train_loader, axes=norm_axes)
-
+        print(f"Calculated mean float32: {mean.flatten()}")
         dataset._norm_params = (mean.to(dataset.precision), std.to(dataset.precision))
+        print("in dataset params", dataset._norm_params[0].flatten(), dataset._norm_params[1].flatten())
     train_dataset = Subset(dataset, train_idx)
     val_dataset = Subset(dataset, val_idx)
     test_dataset = Subset(dataset, test_idx)

test/train_loop.py
@@ -38,7 +38,10 @@ metrics = {}
 dataset = TorchDataset(root_folder=dataset_path, precision=torch.float16)
 
 
+
+
 train_loader, val_loader, _ = get_data_loaders(dataset, sets_size={'train': 0.75, 'val': 0.25, 'test': 0}, norm_axes=(0,2,3,4))
+os.abort()
 print("\nStarting dummy training loop...")
-print(dataset._norm_params)
+
 model = train_model(model, train_loader=train_loader, val_loader=val_loader, loss_criterion=criterion, optimizer=optimizer, config=config, metrics=metrics)