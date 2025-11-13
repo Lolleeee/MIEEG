Added custom plotting, fixed validation metrics wrong scale and history scaling bug

packages/train/helpers.py
@@ -5,7 +5,7 @@ from typing import Callable, Dict, Set
 
 import matplotlib.pyplot as plt
 import torch
-
+import copy
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from packages.plotting import train_plots
 from packages.train.metrics import TorchMetric
@@ -144,7 +144,7 @@ class History(Helper):
         logging.info("Validation" + log_string)
 
     def _end_train_step(self):
-
+        
         self._plot_history()
 
     @staticmethod
@@ -152,9 +152,9 @@ class History(Helper):
         log_string = ""
         for metric_name, metric_value in metrics.items():
             if metric_name in curr_history:
-                curr_history[metric_name].append(metric_value)  # Debug print
+                curr_history[metric_name].append(metric_value) 
                 log_string += f"--- {metric_name}: {metric_value:.4f}"
-
+        
         return curr_history, log_string
 
     def _plot_history(self):
@@ -207,7 +207,19 @@ class LRScheduler(Helper):
             self._last_lr = self.scheduler.get_last_lr()[0]
             logging.info(f"Updated Learning Rate: {self.scheduler.get_last_lr()[0]:.6f}")
 
+class CustomPlotter(Helper):
+    def __init__(self, trainer: "Trainer", plot_function: Callable, plot_function_args: dict, plot_interval: int):
+        self.trainer = trainer
+        self.plot_function = plot_function
+        self.plot_function_args = plot_function_args
+        self.plot_interval = plot_interval
+        self.counter = 0
+        
 
+    def _end_train_epoch_step(self, **kwargs):
+        self.counter += 1
+        if self.counter % self.plot_interval == 0:
+            self.plot_function(trainer=self.trainer, **self.plot_function_args)
 
 
 class HelperHandler:
@@ -218,11 +230,13 @@ class HelperHandler:
     def _call_end_train_epoch_step(self, metrics):
         for helper_instance in self.helpers.values():
             if hasattr(helper_instance, '_end_train_epoch_step'):
+                metrics = copy.deepcopy(metrics)
                 helper_instance._end_train_epoch_step(**metrics)
 
     def _call_end_val_step(self, metrics):
         for helper_instance in self.helpers.values():
             if hasattr(helper_instance, '_end_val_step'):
+                metrics = copy.deepcopy(metrics)
                 helper_instance._end_val_step(**metrics)
 
     def _call_end_train_step(self):
@@ -275,7 +289,13 @@ class HelperHandler:
                         metric=self.trainer.config.helpers.early_stopping.metric,
                         mode=self.trainer.config.helpers.early_stopping.mode
                     )
-
+                case "custom_plotter":
+                    helper_instance = CustomPlotter(
+                        trainer=self.trainer,
+                        plot_function=self.trainer.config.helpers.custom_plotter.get_plot_function,
+                        plot_function_args=self.trainer.config.helpers.custom_plotter.plot_function_args,
+                        plot_interval=self.trainer.config.helpers.custom_plotter.plot_interval
+                    )
                 case _:
                     logging.warning(f"Unknown helper: {helper_name}, skipping initialization.")
                 

packages/train/loss.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from typing import Dict, Union, Tuple, Any, List
 from abc import abstractmethod
 import logging
-from packages.train.torch_wrappers import TorchLoss
+from packages.train.F_torch_wrappers import TorchLoss
 
 
 logging.basicConfig(level=logging.INFO)

packages/train/training.py
@@ -54,13 +54,13 @@ class MetricsHandler:
         
         # Main loss value to be backpropagated
         loss_eval = batch_loss_eval_dict['loss']
-
+        
         # Accumulate loss into metrics dict
         for loss_comp_name, value in batch_loss_eval_dict.items():
             if isinstance(value, torch.Tensor):
                 value = value.detach().cpu()
             self.metrics_eval[loss_comp_name] += value * target.size(0)
-
+            
         # Accumulate metrics
         for metric_name, metric in self.metrics.items():
             value = metric(outputs, target).detach().cpu()
@@ -72,19 +72,19 @@ class MetricsHandler:
         assert self.epoch_samples_count > 0, "No samples were processed in this epoch."
         assert all(value >= 0 for value in self.metrics_eval.values()), "One or more metric evaluations are negative, which is invalid."
         # Scaling metrics by number of samples
+        
         self.metrics_eval = self.scale_loss_dict(self.metrics_eval, 1.0 / self.epoch_samples_count, ignore_keys=['epoch'])
 
         
-    def _new_epoch_setup(self, current_epoch: int = None):
+    def _new_epoch_setup(self, current_epoch: int):
         assert current_epoch is not None, "Current epoch must be provided for new epoch setup."
 
-        if current_epoch != self.metrics_eval['epoch']:
-            self.epoch_samples_count = 0
-            for metrics_name in self.metrics_eval:
-                if metrics_name == "epoch":
-                    self.metrics_eval[metrics_name] = current_epoch
-                else:
-                    self.metrics_eval[metrics_name] = 0.0
+        self.epoch_samples_count = 0
+        for metrics_name in self.metrics_eval:
+            if metrics_name == "epoch":
+                self.metrics_eval[metrics_name] = current_epoch
+            else:
+                self.metrics_eval[metrics_name] = 0.0
 
     def _initialize_metrics_eval_dict(self):
         self.metrics_eval['epoch'] = 0
@@ -269,13 +269,13 @@ class Trainer():
                 self.metrics_handler._new_epoch_setup(epoch)
 
                 self.model.train()
-
+                print("model.train")
                 train_metrics = self._train_epoch()
-
+                
                 self.metrics_handler._new_epoch_setup(epoch)
-
+                
                 val_metrics = self._start_val_loop()
-
+                
                 if 'early_stopping' in self.helper_handler.helpers and self.helper_handler.helpers['early_stopping'].stop_early:
                     logging.info("Early stopping triggered. Ending training loop.")
                     break
@@ -297,7 +297,7 @@ class Trainer():
                 with torch.autocast(device_type=self.device.type, enabled=self.use_amp):
                     outputs = self.model(batch['input'])
                     batch_loss_eval = self.metrics_handler._batch_step(outputs, batch)
-                
+                    
                 self.scaler.scale(batch_loss_eval).backward()
 
                 if self.grad_clip is not None:
@@ -317,7 +317,7 @@ class Trainer():
         return self.metrics_handler.metrics_eval
     
     def _start_val_loop(self):
-        
+        print("model.eval")
         self.model.eval()
 
         with torch.no_grad():
@@ -325,11 +325,11 @@ class Trainer():
                 batch = {k: v.to(self.device) for k, v in batch.items()}
 
                 outputs = self.model(batch['input'])
-
+                
                 self.metrics_handler._batch_step(outputs, batch, detach=True)
-
+                
         self.metrics_handler._end_epoch_step()
-
+        
         self.helper_handler._call_end_val_step(self.metrics_handler.metrics_eval)
 
         return self.metrics_handler.metrics_eval
@@ -346,6 +346,8 @@ class Trainer():
                 outputs = self.model(batch['input'])
 
                 self.metrics_handler._batch_step(outputs, batch, detach=True)
+
+        self.metrics_handler._end_epoch_step()
 
         test_metrics = self.metrics_handler.metrics_eval
         for metric_name, metric_value in test_metrics.items():